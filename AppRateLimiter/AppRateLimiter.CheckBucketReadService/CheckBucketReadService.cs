c3f00e8f35cd49e2f336c5f1e3fbf0a03fd7c1c5
kcq218 <khangquach218@gmail.com>
2024-12-25 13:29:36 -0800

    read for urlshortener

--- AppRateLimiter/AppRateLimiter.CheckBucketReadService/CheckBucketReadService.cs
+++ AppRateLimiter/AppRateLimiter.CheckBucketReadService/CheckBucketReadService.cs
@@ -1,26 +1,48 @@
+using AppRateLimiter.CheckBucketReadService.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AppRateLimiter.ReadService
 {
     public class CheckBucketReadService
     {
         private readonly ILogger<CheckBucketReadService> _logger;
+        private readonly IReadService _readService;
 
-        public CheckBucketReadService(ILogger<CheckBucketReadService> logger)
+        public CheckBucketReadService(ILogger<CheckBucketReadService> logger, IReadService readService)
         {
             _logger = logger;
+            _readService = readService;
         }
 
         [Function("read")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
         {
 
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            try
+            {
+                string url = req.Query["url"];
 
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+                url = url ?? data?.url;
+
+                if (!string.IsNullOrWhiteSpace(url))
+                {
+                    return new OkObjectResult(_readService.ReadAsync(url));
+                }
+
+                return new OkObjectResult("empty body request");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+
+                return new OkObjectResult(e.ToString());
+            }
         }
     }
 }
