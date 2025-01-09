6f5467d81aa277142382b8182eecd25f370b6839
kcq218 <khangquach218@gmail.com>
2025-01-08 23:53:29 -0800

    added null checks to avoid warnings.

--- AppRateLimiter/AppRateLimiter.Test/RefillServiceTest.cs
+++ AppRateLimiter/AppRateLimiter.Test/RefillServiceTest.cs
@@ -8,14 +8,13 @@ namespace AppRateLimiter.Test
     [TestClass]
     public sealed class RefillServiceTest
     {
-        private Mock<IUnitofWork> _MockUnitofWork;
-        private UserBucket _MockUser;
-        private IRefillService _RefillService;
+        private Mock<IUnitofWork>? _MockUnitofWork;
+        private UserBucket? _MockUser;
+        private IRefillService? _RefillService;
 
         [TestInitialize]
         public void Initialize()
         {
-            //_TestData = new TestData();
             _MockUnitofWork = new Mock<IUnitofWork>();
             _MockUnitofWork.Setup(m => m.UserBucketRepository.Update(It.IsAny<UserBucket>())).Verifiable();
             _MockUnitofWork.Setup(m => m.Save()).Verifiable();
@@ -26,19 +25,25 @@ namespace AppRateLimiter.Test
         [TestMethod]
         public void BucketShouldBeRefilled()
         {
-            _MockUser.LastAccessed = DateTime.Now.AddSeconds(-30);
-            _RefillService.RefillBucket(_MockUser);
-            _MockUnitofWork.Verify(m => m.UserBucketRepository.Update(It.IsAny<UserBucket>()), Times.Once);
-            _MockUnitofWork.Verify(m => m.Save(), Times.Once);
+            if (_MockUser != null && _RefillService != null)
+            {
+                _MockUser.LastAccessed = DateTime.Now.AddSeconds(-30);
+                _RefillService.RefillBucket(_MockUser);
+                _MockUnitofWork?.Verify(m => m.UserBucketRepository.Update(It.IsAny<UserBucket>()), Times.Once);
+                _MockUnitofWork?.Verify(m => m.Save(), Times.Once);
+            }
         }
 
         [TestMethod]
         public void BucketShouldNotBeRefilled()
         {
-            _MockUser.LastAccessed = DateTime.Now.AddSeconds(-29);
-            _RefillService.RefillBucket(_MockUser);
-            _MockUnitofWork.Verify(m => m.UserBucketRepository.Update(It.IsAny<UserBucket>()), Times.Never);
-            _MockUnitofWork.Verify(m => m.Save(), Times.Never);
+            if (_MockUser != null && _RefillService != null)
+            {
+                _MockUser.LastAccessed = DateTime.Now.AddSeconds(-29);
+                _RefillService.RefillBucket(_MockUser);
+                _MockUnitofWork?.Verify(m => m.UserBucketRepository.Update(It.IsAny<UserBucket>()), Times.Never);
+                _MockUnitofWork?.Verify(m => m.Save(), Times.Never);
+            }
         }
     }
 }
